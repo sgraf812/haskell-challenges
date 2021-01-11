a9ecb6ac7face78482683996c515559b22dddca2
Sebastian Graf <sebastian.graf@kit.edu>
2021-01-11 12:44:34 +0100

    Solution, but crashes

--- force-elems/src/Lib.hs
+++ force-elems/src/Lib.hs
@@ -1,6 +1,23 @@
+{-# OPTIONS_GHC -ddump-simpl #-}
+
 module Lib
     ( forceElems
+    , forceElemsList
     ) where
 
+import Data.Coerce
+
+newtype ValueStrict a = ValueStrict a
+
+instance Functor ValueStrict where
+  fmap f (ValueStrict a) = ValueStrict (a `seq` f a)
+
+instance Applicative ValueStrict where
+  pure = ValueStrict
+  ValueStrict f <*> ValueStrict a = ValueStrict (f a)
+
 forceElems :: Traversable t => t a -> t a
-forceElems = undefined
+forceElems = coerce . traverse ValueStrict
+
+forceElemsList :: [a] -> [a]
+forceElemsList = forceElems
