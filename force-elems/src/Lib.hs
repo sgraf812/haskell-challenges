fb75383373d05c39e9def1e34dc38eeac36194b0
Sebastian Graf <sebastian.graf@kit.edu>
2021-01-12 13:07:16 +0100

    Got it working

--- force-elems/src/Lib.hs
+++ force-elems/src/Lib.hs
@@ -1,23 +1,45 @@
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
 {-# OPTIONS_GHC -ddump-simpl #-}
 
 module Lib
     ( forceElems
     , forceElemsList
+    -- , forceElemsTree2
+    , forceElemsSnocL
     ) where
 
-import Data.Coerce
+data T a = L a | S a
 
-newtype ValueStrict a = ValueStrict a
+getT :: T a -> a
+getT (L a) = a
+getT (S a) = a
 
-instance Functor ValueStrict where
-  fmap f (ValueStrict a) = ValueStrict (a `seq` f a)
+data SnocL a
+    = Snoc (SnocL a) a | Lin
+    deriving (Functor, Foldable, Traversable)
 
-instance Applicative ValueStrict where
-  pure = ValueStrict
-  ValueStrict f <*> ValueStrict a = ValueStrict (f a)
+data Tree2 a
+    = Fork2 (Tree2 a) a (Tree2 a) a
+    deriving (Functor, Foldable, Traversable)
+
+instance Functor T where
+  fmap f ta = pure f <*> ta
+
+instance Applicative T where
+  pure      = L
+  tf <*> ta = L $
+    case ta of
+      L a -> getT tf a
+      S a -> getT tf $! a
 
 forceElems :: Traversable t => t a -> t a
-forceElems = coerce . traverse ValueStrict
+forceElems = getT . traverse S
 
 forceElemsList :: [a] -> [a]
 forceElemsList = forceElems
+
+forceElemsSnocL :: SnocL a -> SnocL a
+forceElemsSnocL = forceElems
+
+forceElemsTree2 :: Tree2 a -> Tree2 a
+forceElemsTree2 = forceElems
